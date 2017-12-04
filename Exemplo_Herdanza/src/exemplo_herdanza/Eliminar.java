60a2f39f7e644a86de26c62962e3d3f0254f7bfb
serxa <serxa@LAPTOP-82OG6QUN>
2017-12-04 08:47:03 +0100

    Final Cmmit

---
+++ Exemplo_Herdanza/src/exemplo_herdanza/Eliminar.java
@@ -0,0 +1,32 @@
+package exemplo_herdanza;
+
+public class Eliminar {
+String aa;
+int bb;
+float cc;
+
+    public String getAa() {
+        return aa;
+    }
+
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
+
+    public int getBb() {
+        return bb;
+    }
+
+    public void setBb(int bb) {
+        this.bb = bb;
+    }
+
+    public float getCc() {
+        return cc;
+    }
+
+    public void setCc(float cc) {
+        this.cc = cc;
+    }
+
+}
