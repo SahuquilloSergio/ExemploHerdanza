060ee0b3b0d4ae0fcd88b956c6eb6f1a791b6daf
serxa <serxa@LAPTOP-82OG6QUN>
2017-12-04 08:47:15 +0100

    Final Cmmit

---
+++ Exemplo_Herdanza/src/exemplo_herdanza/Exemplo_Herdanza.java
@@ -0,0 +1,11 @@
+package exemplo_herdanza;
+
+
+public class Exemplo_Herdanza {
+
+    public static void main(String[] args) {
+        Estudante E1 = new Estudante("Sergio","Sahuquillo",19,"SRX",9);
+        System.out.println(E1.toString());
+    }
+    
+}
